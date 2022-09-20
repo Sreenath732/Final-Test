82665235bd6985b47922196a85c78dcadce047aa
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-20 10:56:31 +0530

    Update Datalogger.sh

--- Datalogger.sh
+++ Datalogger.sh
@@ -1,14 +1,14 @@
 echo "Dataloggerapi APIs checking with json requests"
 
-status1=$(./clientNew 127.0.0.1 6789 0000000020"[{\"username\":\"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"getFields\"}]")
+status1=$(./clientNew 127.0.0.1 6789 "[{\"username\":\"rushikesh\",\"passcode\":\"finecho@178\",\"authkey\":\"abcd\",\"endpoint\":\"getFields\"}]")
 
 echo $status1
 
-status2=$(./clientNew 127.0.0.1 6789 0000000020"[{\"username\":\"sateesh\",\"passcode\":\"123456\",\"authkey\":\"abcd\",\"endpoint\":\"fetchValues\",\"params\":[{\"start_time\":\"2021-06-23 16:19:04\",\"end_time\":\"2021-06-23 16:24:04\",\"skip\":\"600\",\"param_names\":[\"A1_PD1\",\"A1_PD2\",\"A1_PD5\",\"A1_PD8\"]}]}]")
+status2=$(./clientNew 127.0.0.1 6789 "[{\"username\":\"sateesh\",\"passcode\":\"123456\",\"authkey\":\"abcd\",\"endpoint\":\"fetchValues\",\"params\":[{\"start_time\":\"2021-06-23 16:19:04\",\"end_time\":\"2021-06-23 16:24:04\",\"skip\":\"600\",\"param_names\":[\"A1_PD1\",\"A1_PD2\",\"A1_PD5\",\"A1_PD8\"]}]}]")
 
 echo $status2
 
-status3=$(./clientNew 127.0.0.1 6789 0000000020"[{\"username\":\"sateesh\",\"passcode\":\"123456\",\"authkey\":\"abcd\",\"endpoint\":\"fetchTimestamps\",\"params\":[{\"start_time\":\"2021-06-23 16:03:04\",\"end_time\":\"2021-06-23 16:24:04\",\"param_names\":[{\"name\":\"Header_Len\",\"operation\":\">\",\"value\":\"45\"}]}]")
+status3=$(./clientNew 127.0.0.1 6789 "[{\"username\":\"sateesh\",\"passcode\":\"123456\",\"authkey\":\"abcd\",\"endpoint\":\"fetchTimestamps\",\"params\":[{\"start_time\":\"2021-06-23 16:03:04\",\"end_time\":\"2021-06-23 16:24:04\",\"param_names\":[{\"name\":\"Header_Len\",\"operation\":\">\",\"value\":\"45\"}]}]")
 
 echo $status3
 
